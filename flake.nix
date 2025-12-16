c993929d7ccd3e7a8cfc3e05004144a8032b3c0e
SamuelVanie <samuelmichaelvanie@gmail.com>
2025-12-16 07:42:43 -0500

    ci: another try building flake for devbox

--- flake.nix
+++ flake.nix
@@ -1,21 +1,28 @@
 {
   description = "Nix for cobol-ts-mode";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        cobol-ts-mode = pkgs.emacsPackages.trivialBuild {
+          pname = "cobol-ts-mode";
+          version = "0.1.0";
+          src = ./.;
+        };
       in
         {
-          packages.default = pkgs.emacsPackages.trivialBuild {
-            pname = "cobol-ts-mode";
-            version = "0.1.0";
-            src = ./.;
+          packages = {
+            default = cobol-ts-mode;
+            cobol-ts-mode = cobol-ts-mode;
+          };
+          
+          # This makes it searchable by devbox
+          legacyPackages = {
+            cobol-ts-mode = cobol-ts-mode;
           };
         }
     );
