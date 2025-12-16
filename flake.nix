0f027e50ed883de8e90e0c4487651569215a0412
SamuelVanie <samuelmichaelvanie@gmail.com>
2025-12-16 08:20:18 -0500

    ci: going back. Cannot just revert because of lock file

--- flake.nix
+++ flake.nix
@@ -1,13 +1,29 @@
 {
-  description = "COBOL major mode with tree-sitter support";
-
+  description = "Nix for cobol-ts-mode";
   inputs = {
-    emacs-package-flake.url = "github:mrkkrp/emacs-package-flake";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-
-  outputs = { self, emacs-package-flake }:
-    emacs-package-flake.lib.mkFlake {
-      src = ./.;
-      packageName = "cobol-ts-mode";
-    };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        cobol-ts-mode = pkgs.emacsPackages.trivialBuild {
+          pname = "cobol-ts-mode";
+          version = "0.1.0";
+          src = self;
+        };
+      in
+        {
+          packages = {
+            default = cobol-ts-mode;
+            cobol-ts-mode = cobol-ts-mode;
+          };
+          
+          # This makes it searchable by devbox
+          legacyPackages = {
+            cobol-ts-mode = cobol-ts-mode;
+          };
+        }
+    );
 }
