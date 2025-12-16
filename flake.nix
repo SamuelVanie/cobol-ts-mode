a0e4109276abfb85bc3dbe8e2fe12b1d19516a81
SamuelVanie <samuelmichaelvanie@gmail.com>
2025-12-16 07:31:08 -0500

    feat: Add Nix flake for cobol-ts-mode
    - Introduce flake.nix for Nix integration
    - Define default package using emacsPackages.trivialBuild
    - Set pname to "cobol-ts-mode" and version to "0.1.0"

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Nix for cobol-ts-mode";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+        {
+          packages.default = pkgs.emacsPackages.trivialBuild {
+            pname = "cobol-ts-mode";
+            version = "0.1.0";
+            src = ./.;
+          };
+        }
+    );
+}
